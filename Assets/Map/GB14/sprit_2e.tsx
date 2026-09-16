a66e0d818642a10fe4fe19079817c3b6576e63e7
NicholasDiGiacomo <145500496+NicholasDiGiacomo@users.noreply.github.com>
2026-09-16 09:50:08 -0400

    added map and tileset

---
+++ Assets/Map/GB14/sprit_2e.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="sprit_2e" tilewidth="16" tileheight="16" tilecount="180" columns="20">
+ <image source="sprit_2e.png" width="320" height="144"/>
+</tileset>
